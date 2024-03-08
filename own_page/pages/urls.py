6bd9ab5256db2086e6b599f3e006e3e7a56960a9
farah9990 <ahsfy999@gmail.com>
2024-03-08 20:40:16 +0300

    'update'

--- own_page/pages/urls.py
+++ own_page/pages/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Home.html', views.Home, name='Home'),
+    path('About.html', views.About, name='About'),
+    path('Contact.html', views.Contact, name='Contact'),
+    path('Courses.html', views.Courses, name='Courses'),
+    path('Wep-application​s.html', views.WepApplications, name='Wep-applications'),
+    path('Skills.html', views.Skills, name='Skills'),
+    path('Phone-application​s.html', views.PhoneApplications, name='Phone-applications'),
 
 ]
--- own_page/pages/views.py
+++ own_page/pages/views.py
@@ -4,3 +4,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html', {'name':'farah'})
+
+def Home(request):
+    return render(request, 'pages/Home.html')
+
+def About(request):
+    return render(request, 'pages/About.html')
+
+def Contact(request):
+    return render(request, 'pages/Contact.html')
+
+def Skills(request):
+    return render(request, 'pages/Skills.html')
+
+def Courses(request):
+    return render(request, 'pages/Courses.html')
+
+def WepApplications(request):
+    return render(request, 'pages/Wep-applications.html')
+
+def PhoneApplications(request):
+    return render(request, 'pages/Phone-applications.html')
