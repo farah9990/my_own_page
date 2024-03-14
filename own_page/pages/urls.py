c9accfff8828b4ca2d5bd332177281866c343e4c
farah9990 <ahsfy999@gmail.com>
2024-03-14 15:25:03 +0300

    'update'

--- own_page/pages/urls.py
+++ own_page/pages/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='Home'),
     path('Home.html', views.Home, name='Home'),
     path('About.html', views.About, name='About'),
     path('Contact.html', views.Contact, name='Contact'),
     path('Courses.html', views.Courses, name='Courses'),
-    path('Wep-application​s.html', views.WepApplications, name='Wep-applications'),
+    path('WepApplication.html', views.WepApplications, name='Wep-applications'),
     path('Skills.html', views.Skills, name='Skills'),
-    path('Phone-application​s.html', views.PhoneApplications, name='Phone-applications'),
+    path('PhoneApplication.html', views.PhoneApplications, name='Phone-applications'),
 
 ]
--- own_page/pages/views.py
+++ own_page/pages/views.py
@@ -3,25 +3,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html', {'name':'farah'})
+    return render(request, 'pages/index.html', {'name':'Home'})
 
 def Home(request):
-    return render(request, 'pages/Home.html')
+    return render(request, 'pages/Home.html', {'name':'Home'})
 
 def About(request):
-    return render(request, 'pages/About.html')
+    return render(request, 'pages/About.html',  {'name':'About'})
 
 def Contact(request):
-    return render(request, 'pages/Contact.html')
+    return render(request, 'pages/Contact.html', {'name':'Contact'})
 
 def Skills(request):
-    return render(request, 'pages/Skills.html')
+    return render(request, 'pages/Skills.html', {'name':'Skills'})
 
 def Courses(request):
-    return render(request, 'pages/Courses.html')
+    return render(request, 'pages/Courses.html', {'name':'Courses'})
 
 def WepApplications(request):
-    return render(request, 'pages/Wep-applications.html')
+    return render(request, 'pages/WepApplication.html', {'name':'Wep-applications'})
 
 def PhoneApplications(request):
-    return render(request, 'pages/Phone-applications.html')
+    return render(request, 'pages/PhoneApplication.html', {'name':'Phone-applications'})
